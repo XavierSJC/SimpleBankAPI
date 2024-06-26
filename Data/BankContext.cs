e4a5086c689a2273c94cada46c66169cf10e364e
Alex Xavier <alexsantosxavier@gmail.com>
2024-06-26 16:47:03 -0300

    Adding the simplest model to an account Adding EF to project and a DBContext

---
+++ Data/BankContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBankAPI.Models;
+
+namespace SimpleBankAPI.Data
+{
+    public class BankContext: DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options) : base(options) { }
+        
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Account>().ToTable("Account");
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleBankAPI.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public float balance { get; set; }
+    }
+}
