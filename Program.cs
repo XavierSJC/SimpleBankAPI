87abc9310062ea8bcaf7fea5dcf95641aac92b3c
Alex Xavier <alexsantosxavier@gmail.com>
2024-06-26 18:09:31 -0300

    Using SQLite as a DB provider This DB is in memory.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using SimpleBankAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var conn = new SqliteConnection("DataSource=:memory:");
+conn.Open();
+builder.Services.AddDbContext<BankContext>(opt => opt.UseSqlite(conn));
 
 var app = builder.Build();
 
@@ -22,4 +30,26 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+CreateDbIfNotExist(app);
+
 app.Run();
+
+conn.Close();
+
+void CreateDbIfNotExist(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<BankContext>();
+            context.Database.EnsureCreated();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
