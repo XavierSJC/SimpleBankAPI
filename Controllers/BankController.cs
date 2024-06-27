535127f8d0a257e1e7904c6f9ba1e640aa0cbd00
Alex Xavier <alexsantosxavier@gmail.com>
2024-06-27 16:03:14 -0300

    Added exceptions catch

--- Controllers/BankController.cs
+++ Controllers/BankController.cs
@@ -10,46 +10,64 @@ namespace SimpleBankAPI.Controllers
     public class BankController : ControllerBase
     {
         private readonly ILogger<BankController> _logger;
-        private readonly IBank _bank;
+        private readonly IBank _bankService;
         
-        public BankController(ILogger<BankController> logger, IBank bank)
+        public BankController(ILogger<BankController> logger, IBank bankService)
         {
             _logger = logger;
-            _bank = bank;
+            _bankService = bankService;
         }
 
         [HttpPost("reset")]
         public ActionResult Reset()
         {
-            _bank.Reset();
-            return Ok("OK");
+            try
+            {
+                _bankService.Reset();
+                return Ok("OK");
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("balance")]
-        public ActionResult GetBalance([FromQuery]string account_id) 
-        { 
-            return Ok(_bank.GetBalance(account_id));
+        public ActionResult GetBalance([FromQuery]string account_id)
+        {
+            try
+            {
+                return Ok(_bankService.GetBalance(account_id));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(0);
+            }
         }
 
         [HttpPost("event")]
         public ActionResult Event([FromBody]Event bankEvent)
         {
-            float finalBalance;
-
-            switch (bankEvent.Type)
+            try
             {
-                case "deposit":
-                    return Created(string.Empty, new EventAnswer() { Destination = _bank.Deposit(bankEvent.Destination, bankEvent.Amount) });
+                switch (bankEvent.Type)
+                {
+                    case "deposit":
+                        return Created(string.Empty, new EventAnswer() { Destination = _bankService.Deposit(bankEvent.Destination, bankEvent.Amount) });
 
-                case "withdraw":
-                    return Created(string.Empty, new EventAnswer() { Origin = _bank.Withdraw(bankEvent.Origin, bankEvent.Amount) });
+                    case "withdraw":
+                        return Created(string.Empty, new EventAnswer() { Origin = _bankService.Withdraw(bankEvent.Origin, bankEvent.Amount) });
 
-                case "transfer":
-                    IEnumerable<Account> result = _bank.Transfer(bankEvent.Origin, bankEvent.Destination, bankEvent.Amount);
-                    return Created(string.Empty, new EventAnswer() { Origin = result.ElementAt(0), Destination = result.ElementAt(1) });
+                    case "transfer":
+                        IEnumerable<Account> result = _bankService.Transfer(bankEvent.Origin, bankEvent.Destination, bankEvent.Amount);
+                        return Created(string.Empty, new EventAnswer() { Origin = result.ElementAt(0), Destination = result.ElementAt(1) });
 
-                default:
-                    return BadRequest("Event is not valid");
+                    default:
+                        return BadRequest("Event is not valid");
+                }
+            }catch (Exception ex)
+            {
+                return NotFound(0);
             }
         }
     }
